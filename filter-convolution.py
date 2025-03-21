607aba1e86da2ce26e828eac850e139e8113c01e
Regina Orduño <a01252959@tec.mx>
2025-03-20 19:30:56 -0700

    tres convoluciones

--- filter-convolution.py
+++ filter-convolution.py
@@ -11,12 +11,6 @@ import matplotlib.pyplot as plt
 
 def convolution(image_path, filtro):
 
-    # Cargar la imagen en escala de grises
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    if image is None:
-        print(f"Error: No se pudo cargar la imagen")
-        return None
-
     # Obtener dimensiones
     img_height, img_width = image.shape
     filtro_height, filtro_width = filtro.shape
@@ -31,38 +25,67 @@ def convolution(image_path, filtro):
             output[i, j] = np.sum(region * filtro)
 
     output = np.clip(output, 0, 255).astype(np.uint8)
+
     
-    return image, output  # Devuelve la imagen original y la filtrada
+    return output # Devuelve la imagen original y la filtrada
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    image_path = "Turquia.jpg" 
+    image_path = "dog.jpeg" 
+
+    # Cargar la imagen en escala de grises
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        print(f"Error: No se pudo cargar la imagen")
+        exit()
 
     # Filtro Sobel en X (detección de bordes)
-    filtro = np.array([
+    filtro1 = np.array([
         [-1, 0, 1],
         [-2, 0, 2],
         [-1, 0, 1]
     ], dtype=np.float32)
 
-    # Aplicar convolución
-    original, resultado = convolution(image_path, filtro)
+    filtro2 = np.array([
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]
+    ], dtype=np.float32)
 
-    if resultado is not None:
+    filtro3 = np.array([    
+        [0, 1, 0],
+        [1, -4, 1],
+        [0, 1, 0]
+    ], dtype=np.float32)
+
+    # Aplicar convolución
+    resultado1 = convolution(image, filtro1)
+    resultado2 = convolution(resultado1, filtro2)
+    resultado3 = convolution(resultado2, filtro3)
+
+    if resultado1 is not None:
         # Mostrar las imágenes con Matplotlib
         plt.figure(figsize=(10, 5))
 
         # Imagen original
-        plt.subplot(1, 2, 1)
-        plt.imshow(original, cmap='gray')
+        plt.subplot(1, 4, 1)
+        plt.imshow(image, cmap='gray')
         plt.title("Imagen Original")
         plt.axis("off")
 
-        # Imagen con convolución
-        plt.subplot(1, 2, 2)
-        plt.imshow(resultado, cmap='gray')
+        # Filtro 1
+        plt.subplot(1, 4, 2)
+        plt.imshow(resultado1, cmap='gray')
         plt.title("Imagen con Convolución ")
         plt.axis("off")
 
+        # Filtro 2
+        plt.subplot(1, 4, 3)
+        plt.imshow(resultado2, cmap='gray')
+
+        # Filtro 3
+        plt.subplot(1, 4, 4)
+        plt.imshow(resultado3, cmap='gray')
+        
         # Mostrar la figura
         plt.show()
